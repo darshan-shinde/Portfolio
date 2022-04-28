78de01d70374781cba55f951ead5158a8db9f3fa
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-04-28 22:14:43 +0530

    y

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <br></br>
+            <hr></hr>
+            <p className="footer-text">Made with ♥️ by Avinash K<br></br>
+            <br></br>
+            <small>All rights reserved © 2022 Avinash K</small><br></br>
+            <small>Illustrations by undraw.co</small>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
